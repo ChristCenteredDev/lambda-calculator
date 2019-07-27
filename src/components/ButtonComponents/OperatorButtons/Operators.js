566ba5507e1cd0745b401fd80745b8181c5a7635
William M <wmooney1984@gmail.com>
2019-07-27 00:24:51 -0400

    Numbers and Operator Components Completed

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import operators from "../../../data";
+import OperatorButton from './OperatorButton';
 
-const Operators = () => {
+
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
+  const [operator, setOperator ] = useState(operators);
+
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       { operator.map((operator, index)=>
+        <OperatorButton changeData={props.changeData} operator={operator} key={index} />
+       )};
     </div>
   );
 };
+
+export default Operators;
